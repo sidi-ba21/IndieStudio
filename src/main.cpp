fc3fa9fe6ef497c92bcdf6e016d20c1611d01937
Ariel Ganem <ariel.ganem@epitech.eu>
2022-06-03 11:56:52 +0200

    [add] Map linked with the 3D model char fully working with black background

--- src/main.cpp
+++ src/main.cpp
@@ -24,85 +24,116 @@ int main(void)
 	/**/
 
 	// Load model mesh
-	
+
 	Model model_test = LoadModel("../assets/robo6.iqm");
-	
 
 	// Load model texture
-	
+
 	Texture2D texture = LoadTexture("../assets/txr.png");
 	SetMaterialTexture(&model_test.materials[0], MATERIAL_MAP_DIFFUSE, texture);
-	
 
 	// Load model animation
-	
+
 	unsigned int animsCount = 2;
 	ModelAnimation *anims = LoadModelAnimations("../assets/robo6.iqm", &animsCount);
 	int animFrameCounter = 0;
-	
-	
 
-	SetCameraMode(camera, CAMERA_CUSTOM);
+	ToggleFullscreen();
+	SetCameraMode(camera, CAMERA_FREE);
 
 	//--------------------------------------------------------------------------------------
 	float x = 0;
 	float y = 0;
+	int pos = 10;
+	int size = 20;
+	Vector3 cubePosition = {0.0f, .0f, pos};
+	Vector3 cubePosition1 = {0.0f, .0f, -pos};
+	Vector3 cubePosition2 = {-pos, .0f, 0.0f};
+	Vector3 cubePosition3 = {pos, .0f, 0.0f};
+	Vector3 cubePosition4 = {0.0f, .0f, 0.0f};
+
 	while (!WindowShouldClose())
 	{
+		UpdateCamera(&camera);
+		
 		/* ROTATE CAMERA */
-		if (IsKeyDown('Q')) cam_angle+=0.12;
-		if (IsKeyDown('E')) cam_angle-=0.12;
+		if (IsKeyDown('Q'))
+			cam_angle += 0.12;
+		if (IsKeyDown('E'))
+			cam_angle -= 0.12;
 		camera.position.x = cam_radius * cos(cam_angle);
 		camera.position.z = cam_radius * sin(cam_angle);
 
 		/* UPDATE ANIMATION */
-		
+
 		if (IsKeyDown(KEY_SPACE))
 		{
 			UpdateModelAnimation(model_test, anims[1], animFrameCounter);
 			animFrameCounter++;
-			if (animFrameCounter >= anims[1].frameCount) animFrameCounter = 0;
+			if (animFrameCounter >= anims[1].frameCount)
+				animFrameCounter = 0;
 		}
-		
+
 		if (IsKeyDown(KEY_UP))
 		{
 			UpdateModelAnimation(model_test, anims[0], animFrameCounter);
 			animFrameCounter++;
-			if (animFrameCounter >= anims[0].frameCount) animFrameCounter = 0;
+			if (animFrameCounter >= anims[0].frameCount)
+				animFrameCounter = 0;
 		}
 
 		if (IsKeyDown(KEY_DOWN))
 		{
 			UpdateModelAnimation(model_test, anims[2], animFrameCounter);
 			animFrameCounter++;
-			if (animFrameCounter >= anims[2].frameCount) animFrameCounter = 0;
+			if (animFrameCounter >= anims[2].frameCount)
+				animFrameCounter = 0;
 		}
-		
+
 		BeginDrawing();
 
-			ClearBackground(WHITE);
+		ClearBackground(BLACK);
 
-			BeginMode3D(camera);
+		BeginMode3D(camera);
 
-				DrawModel(model_test, (Vector3){x, 0, y}, 1, WHITE);
+		DrawModel(model_test, (Vector3){x, 0, y}, 1, WHITE);
 
-				DrawGrid(15, 1.0);
-				if (IsKeyDown(KEY_LEFT)) x+=0.02;
-				if (IsKeyDown(KEY_RIGHT)) x-=0.02;
-				if (IsKeyDown(KEY_UP)) y+=0.02;
-				if (IsKeyDown(KEY_DOWN)) y-=0.02;
-			EndMode3D();
+		DrawGrid(size, 1.0f);
+		if (IsKeyDown(KEY_LEFT))
+			x += 0.02;
+		if (IsKeyDown(KEY_RIGHT))
+			x -= 0.02;
+		if (IsKeyDown(KEY_UP))
+			y += 0.02;
+		if (IsKeyDown(KEY_DOWN))
+			y -= 0.02;
 
-			DrawFPS(10, 10);
+		for (float cube = 0.0; cube < size; cube++)
+		{
+			DrawCube(cubePosition, cube + 2, 2.0f, 1.0f, DARKBROWN);
+			DrawCubeWires(cubePosition, cube + 2, 2, 1, BLACK);
+			DrawCube(cubePosition1, cube + 2, 2.0f, 1.0f, DARKBROWN);
+			DrawCubeWires(cubePosition1, cube + 2, 2, 1, BLACK);
+			DrawCube(cubePosition2, 1, 2.0f, cube, DARKBROWN);
+			DrawCubeWires(cubePosition2, 1, 2, cube, BLACK);
+			DrawCube(cubePosition3, 1, 2.0f, cube, DARKBROWN);
+			DrawCubeWires(cubePosition3, 1, 2, cube, BLACK);
+			DrawCube(cubePosition4, cube, 1.0f, cube, BROWN);
+		}
+
+		EndMode3D();
+
+		DrawFPS(10, 10);
 
 		EndDrawing();
 	}
 
 	// FREE MEMORY !! (important)
-	
-	for (unsigned int i = 0; i < animsCount; i++) UnloadModelAnimation(anims[i]);
+
+	for (unsigned int i = 0; i < animsCount; i++)
+		UnloadModelAnimation(anims[i]);
 	RL_FREE(anims);
-	
+
 	UnloadTexture(texture);
 
 	UnloadModel(model_test);
