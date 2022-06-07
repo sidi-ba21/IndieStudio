99d0960a503fb3a193a314937ebb17acfbc7ce0b
Grisou <Grisou@localhost.localdomain>
2022-06-07 16:01:53 +0200

    [UPDATE] core destructor

--- Cmake_script.sh
+++ Cmake_script.sh
@@ -8,6 +8,9 @@ if [ $# -eq 1 ]; then
     if [ $1 = "build" ]; then
         mkdir $CMAKE_FILE && cd $CMAKE_FILE && cmake .. && cmake --build . && cd ..
         echo "Build done."
+    elif [ $1 = "make" ]; then
+        cd build && make && cd ..
+        echo "make done."
     elif [ $1 = "clean" ]; then
         rm -r $CMAKE_FILE && rm $BINARY
         echo "Clean done."
--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -9,14 +9,14 @@
 
 Bomberman::Core::Core()
 {
+    InitWindow(screenWidth, screenHeight, "BOOOOOMBERMAAN");
+    ToggleFullscreen();
     init();
+    game_loop();
 }
 
 void Bomberman::Core::init(void)
 {
-    InitWindow(screenWidth, screenHeight, "BOOOOOMBERMAAN");
-    ToggleFullscreen();
-
     this->model_test = LoadModel("assets/robo6.iqm");
     this->anims = LoadModelAnimations("assets/robo6.iqm", &animsCount);
     this->player = LoadTexture("assets/txr.png");
@@ -28,36 +28,14 @@ void Bomberman::Core::init(void)
     this->texture = LoadTexture("Png/grassbrick_cube.png"); // Load map texture
 
     SetCameraMode(camera, CAMERA_FREE);
-
-    // Load model texture
-
+    SetCameraMode(camera, CAMERA_ORBITAL); // Set an orbital camera mode
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     SetMaterialTexture(&model_test.materials[0], MATERIAL_MAP_DIFFUSE, player);
 
     model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture; // Set map diffuse texture
-//
-    UnloadImage(image); // Unload cubesmap image from RAM, already uploaded to VRAM
-//
-    SetCameraMode(camera, CAMERA_ORBITAL); // Set an orbital camera mode
-//
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-    game_loop();
 
-    //De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadTexture(cubicmap); // Unload cubicmap texture
-    UnloadTexture(texture);  // Unload map texture
-    UnloadModel(model);      // Unload map model
-
-    for (unsigned int i = 0; i < animsCount; i++)
-       UnloadModelAnimation(anims[i]);
-    RL_FREE(anims);
-
-    UnloadModel(model_test);
-    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 }
-
 
 void Bomberman::Core::game_loop()
 {
@@ -74,39 +52,28 @@ void Bomberman::Core::game_loop()
 
         //UPDATE ANIMATION
 
-        if (IsKeyDown(KEY_SPACE))
-        {
+        if (IsKeyDown(KEY_SPACE)) {
             UpdateModelAnimation(model_test, anims[1], animFrameCounter);
             animFrameCounter++;
             if (animFrameCounter >= anims[1].frameCount)
                 animFrameCounter = 0;
         }
-
-        if (IsKeyDown(KEY_UP))
-        {
+        if (IsKeyDown(KEY_UP)) {
             UpdateModelAnimation(model_test, anims[0], animFrameCounter);
             animFrameCounter++;
             if (animFrameCounter >= anims[0].frameCount)
                 animFrameCounter = 0;
         }
-
-        if (IsKeyDown(KEY_DOWN))
-        {
+        if (IsKeyDown(KEY_DOWN)) {
             UpdateModelAnimation(model_test, anims[2], animFrameCounter);
             animFrameCounter++;
             if (animFrameCounter >= anims[2].frameCount)
                 animFrameCounter = 0;
         }
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-
-        DrawModel(model_test, (Vector3){x, 0.1, y}, 1, WHITE);
-        DrawModel(model_test, (Vector3){q, 0.1, v}, 1, WHITE);
-
+        if (IsKeyPressed(KEY_SPACE)) {
+            score = GetRandomValue(1000, 2000);
+            hiscore = GetRandomValue(2000, 4000);
+        }
         if (IsKeyDown(KEY_Q))
             q += 0.02;
         if (IsKeyDown(KEY_E))
@@ -125,22 +92,23 @@ void Bomberman::Core::game_loop()
         if (IsKeyDown(KEY_DOWN))
             y -= 0.02;
 
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+
+        DrawModel(model_test, (Vector3){x, 0.1, y}, 1, WHITE);
+        DrawModel(model_test, (Vector3){q, 0.1, v}, 1, WHITE);
         DrawModel(model, mapPosition, 1.0f, WHITE);
+
         EndMode3D();
 
         DrawTextureEx(cubicmap, (Vector2){screenWidth - cubicmap.width * 4.0f - 20, 20.0f}, 0.0f, 4.0f, WHITE);
         DrawRectangleLines(screenWidth - cubicmap.width * 4 - 20, 20, cubicmap.width * 4, cubicmap.height * 4, GREEN);
-
         DrawText("The map generated is : ", 1410, 20, 30, BLACK);
         //   DrawText(GetTime, 1410, 20, 30, GREEN);
-
         DrawFPS(10, 1060);
-
-        if (IsKeyPressed(KEY_SPACE))
-        {
-            score = GetRandomValue(1000, 2000);
-            hiscore = GetRandomValue(2000, 4000);
-        }
         DrawText(TextFormat("SCORE: %i", score), 860, 110, 40, BLACK);
         DrawText(TextFormat("HI-SCORE: %i", hiscore), 800, 50, 40, RED);
 
@@ -151,4 +119,15 @@ void Bomberman::Core::game_loop()
 
 Bomberman::Core::~Core()
 {
+    UnloadImage(image); // Unload cubesmap image from RAM, already uploaded to VRAM
+    UnloadTexture(cubicmap); // Unload cubicmap texture
+    UnloadTexture(texture);  // Unload map texture
+    UnloadModel(model);      // Unload map model
+
+    for (unsigned int i = 0; i < animsCount; i++)
+       UnloadModelAnimation(anims[i]);
+    RL_FREE(anims);
+
+    UnloadModel(model_test);
+    CloseWindow(); // Close window and OpenGL context
 }
