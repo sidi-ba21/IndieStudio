e7adaab5ba8bcf18105170c391e7fd874b55db30
Grisou <Grisou@localhost.localdomain>
2022-06-07 16:34:27 +0200

    [DELETE] useless files

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Grisou/delivery/2-TEK/INDIE/B-YEP-400-PAR-4-1-indiestudio-bill.on/build/_deps/raylib-src
-# Build directory: /home/Grisou/delivery/2-TEK/INDIE/B-YEP-400-PAR-4-1-indiestudio-bill.on/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
