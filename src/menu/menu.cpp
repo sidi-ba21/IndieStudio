95c677dd8114ea38cbc3c0467c43e902dbb630c5
BIll ON <bill.on@epitech.eu>
2022-06-10 15:22:34 +0200

    [FIX] game start

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -9,10 +9,11 @@
 
 void Bomberman::Menu::game()
 {
-    (void)mousepos;
-    BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("GAME THINGY", 100, 100, 100, BLACK);
+    this->is_game = true;
+    this->is_title = false;
+    //BeginDrawing();
+    //    ClearBackground(RAYWHITE);
+    //    DrawText("GAME THINGY", 100, 100, 100, BLACK);
     //EndDrawing();
 }
 
@@ -137,70 +138,3 @@ void Bomberman::Menu::loop()
 Bomberman::Menu::~Menu()
 {
 }
-
-/*
-int main(void)
-{
-    Vector2 mousepos = { -100.0f, -100.0f };
-    bool is_title = true;
-    int i = -1;
-
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-    InitAudioDevice();
-    Bomberman::ost menu("./ressources/menu.mp3", 10, false);
-    Bomberman::ost sfx("./ressources/explosion8bit.wav", 5, false);
-
-    InitWindow(screenWidth, screenHeight, "INDIE STUDIO");
-    PlayMusicStream(menu.get_ost());
-
-    SetTargetFPS(60);
-    while (!WindowShouldClose()) {
-        screenWidth = GetScreenWidth();
-        screenHeight = GetscreenHeight();
-        UpdateMusicStream(menu.get_ost());
-        UpdateMusicStream(sfx.get_ost());
-        mousepos = GetMousePosition();
-        if ((GetMusicTimePlayed(sfx.get_ost())/GetMusicTimeLength(sfx.get_ost())) > 1)
-            StopMusicStream(sfx.get_ost());
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && is_title == true) {
-            StopMusicStream(sfx.get_ost());
-            PlayMusicStream(sfx.get_ost());
-            for (i = 0; i < 3; i++)
-                if ((mousepos.x > tab[i].coords.first &&
-                mousepos.x < tab[i].coords.first + 100) &&
-                (mousepos.y > tab[i].coords.second &&
-                mousepos.y < tab[i].coords.second + 100)) {
-                    is_title = false;
-                    break;
-                }
-            (i == 3) ? is_title = true : i;
-        }
-        (IsKeyPressed(KEY_T)) ? (is_title = true) : true;
-        if (IsKeyPressed(KEY_SPACE)) {
-            menu.set_pause();
-        }
-        if (IsKeyPressed(KEY_R)) {
-            StopMusicStream(menu.get_ost());
-            PlayMusicStream(menu.get_ost());
-        }
-        if (is_title == true) {
-            BeginDrawing();
-                ClearBackground(RAYWHITE);
-                DrawRectangle(screenWidth/2, 100, 100, 100, RED);
-                DrawText("GAME", screenWidth/2, 100, 20, BLACK);
-                DrawRectangle(screenWidth/4, 700, 100, 100, GREEN);
-                DrawText("OPTIONS", screenWidth/4, 700, 20, BLACK);
-                DrawRectangle(screenWidth/6, 400, 100, 100, BLUE);
-                DrawText("EXIt", screenWidth/6, 400, 20, BLACK);
-
-                DrawRectangleLines(screenWidth/4*2 - 40, 320, 80, 60, GREEN);
-
-                DrawLine(18, 42, screenWidth - 18, 42, BLACK);
-            EndDrawing();
-        } else 
-            tab[i].lt2(menu, mousepos);
-    }
-    CloseAudioDevice();
-    CloseWindow();
-    return 0;
-}*/
