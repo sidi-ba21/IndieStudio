5da6239f301d0c05224af3bd17b377d1d18d3e5d
Ethan Hayot <ethan.hayot@epitech.eu>
2022-06-10 16:16:23 +0200

    [ADD] AI move perso

--- src/AI/AI.cpp
+++ src/AI/AI.cpp
@@ -7,17 +7,47 @@
 
 #include "AI.hpp"
 #include <memory>
-#include "Error.hpp"
 #include <list>
 
-/*AI::AI(std::function<void (const size_t pos, const std::string action)> playerActionsFunc, const std::vector<AI> &AIs, std::vector<std::shared_ptr<Bomberman::Object>> entities, const std::vector<std::string> &map, const size_t &level)
-        : _playerActionsFunc(playerActionsFunc), _AIs(AIs), _entities(entities), _map(map), _level{level}
+Bomberman::AI::AI()
 {
-    if (AIs.empty())
-        throw AIError("ERROR: AI is empty");
-}*/
+    _Pos2 = get_pos(2);
+    _Model2 = get_Model2();
+    _Anim2 = get_anim2();
+    _AnimFrameCounter = get_anifram();
+}
 
-AI::AI()
+float Bomberman::AI::move_AI()
 {
-	
+    //_time = ;
+    UpdateModelAnimation(_Model2, _Anim2[0], _AnimFrameCounter);
+    _AnimFrameCounter++;
+    if (_AnimFrameCounter >= _Anim2[0].frameCount)
+        _AnimFrameCounter = 0;
+    _Pos2.z -= 20;
+    Rotation2 = 180;
+    setPos(_Pos2);
+
+    /*UpdateModelAnimation(_Model2, _Anim2[0],_AnimFrameCounter);
+    _AnimFrameCounter++;
+    if (_AnimFrameCounter >= _Anim2[0].frameCount)
+       _AnimFrameCounter = 0;
+    _Pos2.z += 0.05;
+    Rotation2 = 0;
+
+    UpdateModelAnimation(_Model2, _Anim2[0],_AnimFrameCounter);
+    _AnimFrameCounter++;
+    if (_AnimFrameCounter >= _Anim2[0].frameCount)
+       _AnimFrameCounter = 0;
+    _Pos2.x -= 0.05;
+    Rotation2 = -90;
+
+    UpdateModelAnimation(_Model2, _Anim2[0],_AnimFrameCounter);
+   _AnimFrameCounter++;
+    if (_AnimFrameCounter >= _Anim2[0].frameCount)
+       _AnimFrameCounter = 0;
+    _Pos2.x += 0.05;
+    Rotation2 = 90;
+
+    return Rotation2;*/
 }
--- src/AI/AI.hpp
+++ src/AI/AI.hpp
@@ -11,26 +11,26 @@
 #include <chrono>
 #include <functional>
 #include <vector>
-#include "../Player/Player.hpp"
-#include "Object.hpp"
-#include "operator.hpp"
 #include <iostream>
+#include "../games/Player/Player.hpp"
 
-class AI: public Bomberman::Player {
-    public:
-        //AI(std::function<void (const size_t pos, const std::string action)> playerActionsFunc, const std::vector<AI> &AIs, std::vector<std::shared_ptr<Bomberman::Object>> entities, const std::vector<std::string> &map, const size_t &level);
-        AI();
-        ~AI() = default;
-        void run(void) const;
-    protected:
-    private:
-    std::vector<AI> _AIs;
-        std::vector<std::shared_ptr<Bomberman::Object>> _entities;
-        std::vector<std::string> _map;
-        size_t _level;
-        std::function<void (const size_t pos, const std::string action)> _playerActionsFunc;
-        float calculateDistance(const Point &start, const Point &end) const;
-        std::string convertPointIntoAction(const std::pair<int, int> &point) const;
-};
+namespace Bomberman
+{
+    class AI: public Bomberman::Player {
+        public:
+            AI();
+            ~AI() = default;
+            float move_AI();
+        protected:
+        private:
+            Vector3 _Pos2;
+            Vector3 _Chk_pos;
+            Model _Model2;
+            ModelAnimation *_Anim2;
+            float Rotation2{0};
+            int _AnimFrameCounter{0};
+            std::chrono::seconds _time;
+    };
+}
 
 #endif /* !AI_HPP_ */
