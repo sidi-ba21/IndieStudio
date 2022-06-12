6df98c2a2a2a78649a05577dbff9ab0255c2bc14
Sidi BA <sidi1.ba@epitech.eu>
2022-06-12 17:03:55 +0200

    [ADD] update highScore

--- src/games/Score/Score.hpp
+++ src/games/Score/Score.hpp
@@ -7,9 +7,10 @@
 
 #include "raylib.h"
 #include <cmath>
+#include <fstream>
 
 #ifndef SCORE_HPP_
-#define SCORE_HPP_
+    #define SCORE_HPP_
 
 namespace Bomberman {
 
@@ -23,11 +24,44 @@ namespace Bomberman {
             int get_score1();
             int get_score2();
             int get_highscore();
+            void loadScore() {
+            std::ifstream file;
+            std::string line;
+            file.open("Score.txt", std::ifstream::in);
+            if (file.peek() == std::ifstream::traits_type::eof())
+                _highscore.assign("NOT AVAILABLE");
+            else {
+                if (file.is_open() && std::getline(file, line))
+                    _highscore = line;
+            }
+            file.close();
+        }
+        void writeScore(std::string score, std::string name) {
+            std::fstream file;
+            std::string line;
+            std::string tmp;
+            (void) name;
+            if (_highscore.compare("NOT AVAILABLE") == 0 || std::stoi(_highscore) < std::stoi(score)) {
+                file.open("Score.txt",  std::ios::trunc | std::ios::in | std::ios::out);
+                file << score << std::endl;
+                _highscore = score;
+            }
+            file.close();
+        };
+      /*  void setPath(const std::string &path) {
+            _path = path;
+        }
+        std::string getName() {
+            return _name;
+        }*/
+        std::string getHightScore() {
+            return _highscore;
+        }
 
         protected:
         private:
             int _score1{0};
-            int _highscore1{0};
+            std::string _highscore{ "NOT AVAILABLE" };
             int _score2{0};
     };
 
