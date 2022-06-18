ab1d7319edc16f3ad7ae81b644c9515fa249adc0
Ariel Ganem <ariel.ganem@epitech.eu>
2022-06-18 15:08:57 +0200

    [add] in game paused menu when the keybind P is pressed

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -26,13 +26,15 @@ void Bomberman::Core::init(void)
     _camera.init();
     _menu.init();
     _box.init();
-    SetTargetFPS(60); 
+    SetTargetFPS(60);
 }
 
 void Bomberman::Core::game_loop()
 {
-    while (!WindowShouldClose()) {
-        if (_menu.get_pause() == false) {
+    while (!WindowShouldClose())
+    {
+        if (_menu.get_pause() == false)
+        {
             _ai.move_AI(_map.get_color(), _map.get_cubicTexture());
             _player.Player_move1(_map.get_color(), _map.get_cubicTexture());
             _player.Player_move2(_map.get_color(), _map.get_cubicTexture());
@@ -51,15 +53,18 @@ void Bomberman::Core::Draw()
 
     _background.draw();
     ClearBackground(RAYWHITE);
-    if (_menu.get_game() == true) {
-        Draw2d();
-        Draw3d();
+    if (_menu.get_game() == true)
+    {
         if (_menu.get_pause() == true)
             _menu.pause();
+        else
+        {
+            Draw2d();
+            Draw3d();
+        }
     }
     else
         _menu.loop();
-
     EndDrawing();
 }
 
@@ -87,7 +92,6 @@ void Bomberman::Core::Draw_map()
 {
     DrawTextureEx(_map.get_cubicTexture(), (Vector2){screenWidth - _map.get_cubicTexture().width * 4.0f - 20, 20.0f}, 0.0f, 4.0f, WHITE);
     DrawRectangleLines(screenWidth - _map.get_cubicTexture().width * 4 - 20, 20, _map.get_cubicTexture().width * 4, _map.get_cubicTexture().height * 4, GREEN);
-
 }
 
 void Bomberman::Core::Draw2d()
@@ -100,17 +104,22 @@ void Bomberman::Core::Draw2d()
 
 void Bomberman::Core::Draw_breakabke()
 {
-    for (int y = 0; y < 16; y++) {
-        for (int x = 0; x < 32; x++) {
-            if (COLOR_EQUAL(_map.get_color()[y * this->_map.get_cubicTexture().width + x], RED)) {
+    for (int y = 0; y < 16; y++)
+    {
+        for (int x = 0; x < 32; x++)
+        {
+            if (COLOR_EQUAL(_map.get_color()[y * this->_map.get_cubicTexture().width + x], RED))
+            {
                 DrawCubeTexture(_box.get_breakable_texture(), Vector3{x - 16.0f, 0.5, y - 8.f},
                                 1, 1, 1, WHITE);
             }
-            if (COLOR_EQUAL(_map.get_color()[y * this->_map.get_cubicTexture().width + x], BLACK)) {
+            if (COLOR_EQUAL(_map.get_color()[y * this->_map.get_cubicTexture().width + x], BLACK))
+            {
                 DrawCubeTextureRec(_box.get_grass_texture(), _box.get_rectGrass(), {x - 16.0f, 0.1, y - 8.f},
                                    1, 0, 1, WHITE);
             }
-            if (COLOR_EQUAL(_map.get_color()[y * this->_map.get_cubicTexture().width + x], WHITE)) {
+            if (COLOR_EQUAL(_map.get_color()[y * this->_map.get_cubicTexture().width + x], WHITE))
+            {
                 DrawCubeTexture(_box.get_brick_texture(), {x - 16.0f, 0.5, y - 8.f},
                                 1, 1, 1, WHITE);
             }
@@ -122,24 +131,29 @@ void Bomberman::Core::Remove_breakable(Vector3 pos)
 {
     int x = pos.x + 16;
     int y = pos.z + 8;
-    
-    if (COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x], RED) || COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x], BLACK)) {
+
+    if (COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x], RED) || COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x], BLACK))
+    {
         _map.get_color()[y * _map.get_cubicTexture().width + x] = BLACK;
         DrawCube(Vector3{(float)(x - 16), _bomb_pos.y, (float)(y - 8)}, 1, 1, 1, RED);
     }
-    if (x < 15 && (COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x + 1], RED) || COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x + 1], BLACK))) {
+    if (x < 15 && (COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x + 1], RED) || COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x + 1], BLACK)))
+    {
         _map.get_color()[y * _map.get_cubicTexture().width + x + 1] = BLACK;
         DrawCube(Vector3{(float)(x + 1 - 16), _bomb_pos.y, (float)(y - 8)}, 1, 1, 1, RED);
     }
-    if (x > -15 && (COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x - 1], RED) || COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x - 1], BLACK))) {
+    if (x > -15 && (COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x - 1], RED) || COLOR_EQUAL(_map.get_color()[y * _map.get_cubicTexture().width + x - 1], BLACK)))
+    {
         _map.get_color()[y * _map.get_cubicTexture().width + x - 1] = BLACK;
         DrawCube(Vector3{(float)(x - 1 - 16), _bomb_pos.y, (float)(y - 8)}, 1, 1, 1, RED);
     }
-    if (y < 7 && (COLOR_EQUAL(_map.get_color()[(y + 1) * _map.get_cubicTexture().width + x], RED) || COLOR_EQUAL(_map.get_color()[(y + 1) * _map.get_cubicTexture().width + x], BLACK))) {
+    if (y < 7 && (COLOR_EQUAL(_map.get_color()[(y + 1) * _map.get_cubicTexture().width + x], RED) || COLOR_EQUAL(_map.get_color()[(y + 1) * _map.get_cubicTexture().width + x], BLACK)))
+    {
         _map.get_color()[(y + 1) * _map.get_cubicTexture().width + x] = BLACK;
         DrawCube(Vector3{(float)(x - 16), _bomb_pos.y, (float)(y + 1 - 8)}, 1, 1, 1, RED);
     }
-    if (y > -7 && (COLOR_EQUAL(_map.get_color()[(y - 1) * _map.get_cubicTexture().width + x], RED) || COLOR_EQUAL(_map.get_color()[(y - 1) * _map.get_cubicTexture().width + x], BLACK))) {
+    if (y > -7 && (COLOR_EQUAL(_map.get_color()[(y - 1) * _map.get_cubicTexture().width + x], RED) || COLOR_EQUAL(_map.get_color()[(y - 1) * _map.get_cubicTexture().width + x], BLACK)))
+    {
         _map.get_color()[(y - 1) * _map.get_cubicTexture().width + x] = BLACK;
         DrawCube(Vector3{(float)(x - 16), _bomb_pos.y, (float)(y - 1 - 8)}, 1, 1, 1, RED);
     }
@@ -149,7 +163,8 @@ void Bomberman::Core::set_Bomb_AI()
 {
     if (pressed_AI)
         _time_bomb += _time.getTime();
-    if (!pressed_AI) {
+    if (!pressed_AI)
+    {
         _bomb_pos_AI = _ai.get_pos();
         pressed_AI = 1;
     }
@@ -157,7 +172,8 @@ void Bomberman::Core::set_Bomb_AI()
         Draw_bomb(_bomb_pos_AI);
     if (_time_bomb > 3 && _time_bomb < 3.5)
         Remove_breakable(_bomb_pos_AI);
-    if (_time_bomb > 6) {
+    if (_time_bomb > 6)
+    {
         _score.update_AI();
         pressed_AI = 0;
         _time_bomb = 0;
@@ -166,10 +182,11 @@ void Bomberman::Core::set_Bomb_AI()
 
 void Bomberman::Core::set_bomb_player()
 {
-    //Player 1
+    // Player 1
     if (pressed)
         _time_bomb2 += _time.getTime();
-    if (IsKeyPressed(KEY_RIGHT_SHIFT) && !pressed) {
+    if (IsKeyPressed(KEY_RIGHT_SHIFT) && !pressed)
+    {
         _bomb_pos = _player.get_pos(1);
         pressed = 1;
     }
@@ -177,13 +194,14 @@ void Bomberman::Core::set_bomb_player()
         Draw_bomb(_bomb_pos);
     if (_time_bomb2 > 3 && _time_bomb2 < 3.5)
         Remove_breakable(_bomb_pos);
-    if (_time_bomb2 > 6) {
+    if (_time_bomb2 > 6)
+    {
         _score.update1();
         pressed = 0;
         _time_bomb2 = 0;
     }
 
-    //Player 2
+    // Player 2
     if (pressed2)
         _time_bomb3 += _time.getTime();
     if ((IsGamepadButtonDown(0, GAMEPAD_BUTTON_RIGHT_FACE_DOWN) || IsKeyPressed(KEY_LEFT_SHIFT)) && !pressed2)
@@ -195,7 +213,8 @@ void Bomberman::Core::set_bomb_player()
         Draw_bomb(_bomb_pos2);
     if (_time_bomb3 > 3 && _time_bomb3 < 3.5)
         Remove_breakable(_bomb_pos2);
-    if (_time_bomb3 > 6) {
+    if (_time_bomb3 > 6)
+    {
         _score.update1();
         _time_bomb3 = 0;
         pressed2 = 0;
