5b45010bea4fe36f8f61334a8552686a53766973
Ethan Hayot <ethan.hayot@epitech.eu>
2022-06-19 23:45:13 +0200

    [FIX] main

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -14,7 +14,7 @@ int basic_error(int ac, char **av)
 {
     (void) av;
     if (ac != 1) {
-        throw FileWrongError("Bomberman: Usage: ./Bomberman");
+        std::cout << "Bomberman: Usage: ./Bomberman" << std::endl;
         return 84;
     }
     return 0;
@@ -22,14 +22,8 @@ int basic_error(int ac, char **av)
 
 int main(int ac, char **av)
 {
-    try {
-        basic_error(ac, av);
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
+    if (basic_error(ac, av) == 84)
         return 84;
-    }
-    //auto time1 = std::time(nullptr);
-    //std::cout << time1 << std::endl;
     Bomberman::Core core;
     return 0;
 }
